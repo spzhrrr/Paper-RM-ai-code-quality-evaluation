8ab6106243aac1f859ba7f93dccde1fc441c2ff0
spzhrrr <syakirapzahra@gmail.com>
2026-04-24 10:26:00 +0700

    Update test_is_prime.py

--- experiment/test/test_is_prime.py
+++ experiment/test/test_is_prime.py
@@ -1,1 +1,19 @@
+import pytest
 
+def test_less_than_two():
+    from solution import is_prime
+    assert is_prime(1) is False
+    assert is_prime(0) is False
+    assert is_prime(-3) is False
+
+def test_primes():
+    from solution import is_prime
+    assert is_prime(2) is True
+    assert is_prime(3) is True
+    assert is_prime(13) is True
+
+def test_composites():
+    from solution import is_prime
+    assert is_prime(4) is False
+    assert is_prime(9) is False
+    assert is_prime(100) is False
